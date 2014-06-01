16b4c74c503860be458e634b90d08bd82456065c
Josh <JoshTheEnder@users.noreply.github.com>
2014-06-01 19:22:46 +0100

    Create server.lua
    Currently only determines whether it's the MASTER or SLAVE but it works

---
+++ server.lua
@@ -0,0 +1,50 @@
+--@name DNS Server
+--@author JoshTheEnder
+--@Desc like real world DNS servers
+
+local component = require("component")
+local event = require("event")
+
+local NameTable = { }
+if component.isAvailable( "modem" ) then
+    m = component.modem
+else
+    error( "Y U NO GIVE ME MODEM!" )
+end
+if m.isWireless() then
+    print("It appears the primary network card of this computer is wireless")
+    print("Beware of duplicate messages / network loops when using this program")
+end
+m.open( 53 )
+m.broadcast( 53, "DNS WHOMASTER" )
+local eventN, _, from, port, _, message = event.pull(5, "modem_message")
+if eventN ~= nil then
+    if (port==53) and (message=="IAMMASTER") then
+        WhoAmI = "SLAVE"
+        MY_MASTER = from
+    end
+else
+    WhoAmI = "MASTER"
+end
+
+print("I have assumed the role of "..WhoAmI)
+
+function WittyFunctionNameHere( LAddr, RAddr, P, Data )
+    if WhoAmI == "MASTER" then
+        if (P==53) and (Data == "DNS WHOMASTER") then
+            print("Received WHOMASTER query from "..RAddr..". Telling them that I am the master...")
+        	m.send(RAddr, P, "IAMMASTER")
+        end
+    end
+end
+
+
+while true do
+	_, LAddr, RAddr, P, _, Data = event.pull( "modem_message" )
+	WittyFunctionNameHere( LAddr, RAddr, P, Data)
+end
+
+
+print( WhoAmI )
+
+print("End Of File")

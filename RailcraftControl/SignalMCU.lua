1b756e296d435f377f2b53d69dc4c588ac25957f
Lizzy <LizzyTrickster@users.noreply.github.com>
2016-05-09 09:03:50 +0100

    Create SignalMCU.lua

---
+++ RailcraftControl/SignalMCU.lua
@@ -0,0 +1,42 @@
+rs = component.proxy( component.list("redstone")() );
+nc = component.proxy( component.list("modem")() );
+--ch = component.proxy( component.list("chat")() );
+si = component.proxy( component.list("sign")() )
+local sides = { bottom = 0, top = 1, back = 2, front = 3, right = 4, left = 5 }
+local aspects = { "red", "yellow", "d-yellow", "green" }
+local direct = { "towards", "away"}
+ 
+ID = si.getValue() -- Sign should be on the font of the MCU and have a 2 digit number on the first line
+if ID == nil then error("No sign, you tit!", 0) end
+ID = ID:sub(1,2)
+nc.open(2222)
+function decodeNetwork( e, l, r, p, d, ... )
+  local excess = table.pack( ... )
+  return e, l, r, p, d, excess
+end
+ 
+ 
+while true do
+  a = table.pack( computer.pullSignal() )
+  if a[1] == "modem_message" then
+    e,l,r,p,d,m = decodeNetwork( table.unpack( a ) ) -- Parameters: EventName, LocalAddress, RemoteAddress, Port, Distance, Message
+    if m[1]:sub(1,2) == ID then -- m[1] will be a number represented in string form like "3212"
+      dir,asp = tonumber(m[1]:sub(3,3)), tonumber( m[1]:sub(4,4) )
+      if dir == 1 then
+        rs.setOutput(sides.right, asp )
+      elseif dir == 0 then
+        rs.setOutput(sides.left, asp )
+      end
+      si.setValue( string.format("%s\nLast Message\n%q\n%q", ID, direct[dir+1], aspects[asp] ) )
+    end
+  end
+end
+
+
+Num = 3212
+--[[
+DIR-FROM-INT: 0-3 = South,west,north,east
+ZONE-BARRIER#: 1-whatever (only used for sections inside the facility, probably)
+DIR-OF-TRVL: 0=towards, 1=away
+ASPECT: 1-4 = most2least restrictive (this is redstone strength, if its 0, the signal will be flashing red+yel)
+]]

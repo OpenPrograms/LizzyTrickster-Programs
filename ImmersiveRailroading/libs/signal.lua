8d130961b324bd6457d05e98563cddc804ab17b1
Lizzy <lizzy@theender.net>
2019-01-06 02:33:11 +0000

    Woo, it now mostly works
    block updates don't seem to get the signals able to transmit updates properly, will need to investigate more.
    
    Thanks to Payonel of the help in trying to work stuff out

--- ImmersiveRailroading/libs/signal.lua
+++ ImmersiveRailroading/libs/signal.lua
@@ -13,6 +13,7 @@ end
 local misc = {}
 
 function misc.stringSplit(inputstr, sep) -- stolen from stack overflow, will move to own file later
+    -- text.split 
     if sep == nil then
             sep = "%s"
     end
@@ -24,7 +25,7 @@ function misc.stringSplit(inputstr, sep) -- stolen from stack overflow, will mov
     return t
 end
 
-function misc.uuid()
+function misc.uuid() -- require("uuid")
     local r = math.random
     return string.format("%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",
     r(0,255),r(0,255),r(0,255),r(0,255),
@@ -88,80 +89,80 @@ function signal.getRedVals (aspect) -- maybe convert these into an addressable f
     return sig[aspect]
 end
 
-function signal:updateAspect ()
-    -- local as 
-    -- if type(aspect) == "string" then
-    --  if self.aspects[string.upper(aspect)] == nil then
-    --      error("Invalid parameter: "..string.upper(aspect)..", expected 'dyffu;")
-    --  else 
-    --      as = self.aspects[string.upper(aspect)]
-    --  end
-    -- elseif type(aspect) == "number" then
-    --  if not (aspect >=1 and aspect <= 4) then
-    --      error("Invalid parameter: "..aspect..", expected range 1-4")
-    --  else
-    --      as = aspect
-    --  end
-    -- else
-    --  error("Expected number or string, got "..type(aspect))
-    -- end
-    d( s({OR=self.override, Si=self.rSide, GG=self.getRedVals(self.state)}) )
-    if self.override then self.redstone.setBundledOutput(self.rSide, self.getRedVals(self.aspects.DANGER)) return end
+-- function signal:updateAspect ()
+--     -- local as 
+--     -- if type(aspect) == "string" then
+--     --  if self.aspects[string.upper(aspect)] == nil then
+--     --      error("Invalid parameter: "..string.upper(aspect)..", expected 'dyffu;")
+--     --  else 
+--     --      as = self.aspects[string.upper(aspect)]
+--     --  end
+--     -- elseif type(aspect) == "number" then
+--     --  if not (aspect >=1 and aspect <= 4) then
+--     --      error("Invalid parameter: "..aspect..", expected range 1-4")
+--     --  else
+--     --      as = aspect
+--     --  end
+--     -- else
+--     --  error("Expected number or string, got "..type(aspect))
+--     -- end
+--     d( s({OR=self.override, Si=self.rSide, GG=self.getRedVals(self.state)}) )
+--     if self.override then self.redstone.setBundledOutput(self.rSide, self.getRedVals(self.aspects.DANGER)) return end
 
-    -- if as == self.state then return end -- if we get told the same state as we already were, we don't need to change
+--     -- if as == self.state then return end -- if we get told the same state as we already were, we don't need to change
     
-    -------------------------
+--     -------------------------
 
-    d( s(rs.setBundledOutput(self.rSide, self.getRedVals(self.state))) )
-    computer.pushSignal("X-AspectChanged", self.id, self.state, true) -- notify other signals we changed
-end
+--     d( s(rs.setBundledOutput(self.rSide, self.getRedVals(self.state))) )
+--     computer.pushSignal("X-AspectChanged", self.id, self.state, true) -- notify other signals we changed
+-- end
 
-function signal:handleBlockUpdate (event_name, block_id, occupied, is_local) 
-    -- need look into why the parameters are to the left by 1
-    d( s({event_name, block_id, occupied, is_local}))
-    if event_name ~= BLOCK_UPDATE_SIGNAL then d("WRONG EVENT BLOCK") return end
-    os.sleep(0.2)
-    --if block_id ~= self.block then d("NOTMYBLOCK") return end
-    d(s({self.override, occupied}))
-    if occupied then 
-        self.override = true
-    else
-        self.override = false
-    end
-    d(s({self.override, occupied}))
-    self:updateAspect()
-end
+-- function signal:handleBlockUpdate (event_name, block_id, occupied, is_local) -- remove this and set it up to be created in :new()
+--     -- need look into why the parameters are to the left by 1
+--     d( s({event_name, block_id, occupied, is_local}))
+--     if event_name ~= BLOCK_UPDATE_SIGNAL then d("WRONG EVENT BLOCK") return end
+--     os.sleep(0.2)
+--     --if block_id ~= self.block then d("NOTMYBLOCK") return end
+--     d(s({self.override, occupied}))
+--     if occupied then 
+--         self.override = true
+--     else
+--         self.override = false
+--     end
+--     d(s({self.override, occupied}))
+--     self:updateAspect()
+-- end
 
-function signal:handleAspect (event_name, signal_id, aspect, is_local)
-    d( s({event_name, signal_id, aspect, is_local}))
-    if event_name ~= ASPECT_UPDATE_SIGNAL then d("WRONG EVENT SIGNAL") return end
-    if signal_id == self.id then d("it's me@") return end -- don't need to update for ourselves
-    if signal_id ~= self.upstream then d("NOTMYUPSTREM") return end -- if it's not our upstream, we dont care
-    if signal_id == self.upstream then -- sanity check
-        d(s({aspect, self.state}))
-        if aspect == 4 then -- if it's already CLEAR upstream, we don't need to do any math
-            self.state = 4
-            self:updateAspect()
-        elseif aspect >= 1 and aspect <= 3 then -- this could be simpler, but sanity checking
-            self.state = aspect + 1
-            d("state is now "..self.state)
-            self:updateAspect()
-        end
-    end
-end
-
-
-function signal:setupEventHandlers ()
-    print("BLOCK", event.listen(BLOCK_UPDATE_SIGNAL, self.handleBlockUpdate)) -- event name pending
-    print("SIGNA", event.listen(ASPECT_UPDATE_SIGNAL, self.handleAspect)) --handle both local and remote aspect in single func for now
-    --event.listen("")
-end
+-- function signal:handleAspect (event_name, signal_id, aspect, is_local) -- remove this and set it up to be created in :new()
+--     d( s({event_name, signal_id, aspect, is_local}))
+--     if event_name ~= ASPECT_UPDATE_SIGNAL then d("WRONG EVENT SIGNAL") return end
+--     if signal_id == self.id then d("it's me@") return end -- don't need to update for ourselves
+--     if signal_id ~= self.upstream then d("NOTMYUPSTREM") return end -- if it's not our upstream, we dont care
+--     if signal_id == self.upstream then -- sanity check
+--         d(s({aspect, self.state}))
+--         if aspect == 4 then -- if it's already CLEAR upstream, we don't need to do any math
+--             self.state = 4
+--             self:updateAspect()
+--         elseif aspect >= 1 and aspect <= 3 then -- this could be simpler, but sanity checking
+--             self.state = aspect + 1
+--             d("state is now "..self.state)
+--             self:updateAspect()
+--         end
+--     end
+-- end
 
 
-function signal:new (signal_id, redstone_comp, side, upstream_signal_id, block_id)
-    o = {}
-    setmetatable(o, self)
-    self.__index = self
+-- function signal:setupEventHandlers () -- todo remove, combine in :new()
+--     print("BLOCK", event.listen(BLOCK_UPDATE_SIGNAL, self.handleBlockUpdate)) -- event name pending
+--     print("SIGNA", event.listen(ASPECT_UPDATE_SIGNAL, self.handleAspect)) --handle both local and remote aspect in single func for now
+--     --event.listen("")
+-- end
+
+
+function signal.new (signal_id, redstone_comp, side, upstream_signal_id, block_id)
+    local o = {}
+    setmetatable(o, signal)
+    signal.__index = signal
     o.id = signal_id
     o.redstone = component.proxy(redstone_comp)
     o.rSide = side
@@ -169,8 +170,56 @@ function signal:new (signal_id, redstone_comp, side, upstream_signal_id, block_i
     o.state = 4
     o.override=false
     o.block = block_id
-    o:setupEventHandlers()
+    --o:setupEventHandlers()
+
+    o.updateAspect = function() 
+        d( s({OR=o.override, Si=o.rSide, GG=o.getRedVals(o.state)}) )
+        if o.override then 
+            o.redstone.setBundledOutput(o.rSide, o.getRedVals(o.aspects.DANGER)) 
+        else
+            d( s(o.redstone.setBundledOutput(o.rSide, o.getRedVals(o.state))) )
+        end
+        computer.pushSignal("X-AspectChanged", o.id, o.state, true) -- notify other signals we changed
+    end
+
+    o.handleAspect = function(event_name, signal_id, aspect, is_local)
+        d( s({event_name, signal_id, aspect, is_local}))
+        if event_name ~= ASPECT_UPDATE_SIGNAL then d("WRONG EVENT SIGNAL") return end
+        if signal_id == o.id then d("it's me") return end -- don't need to update for ourselves
+        if signal_id ~= o.upstream then d("NOTMYUPSTREM") return end -- if it's not our upstream, we dont care
+        if signal_id == o.upstream then -- sanity check
+            d(s({aspect, o.state}))
+            if aspect == 4 then -- if it's already CLEAR upstream, we don't need to do any math
+                o.state = 4
+                o.updateAspect()
+            elseif aspect >= 1 and aspect <= 3 then -- this could be simpler, but sanity checking
+                o.state = aspect + 1
+                d("state is now "..o.state)
+                o.updateAspect()
+            end
+        end
+    end
+
+    o.handleBlockUpdate = function(event_name, block_id, occupied, is_local)
+        d( s({event_name, block_id, occupied, is_local}))
+        if event_name ~= BLOCK_UPDATE_SIGNAL then d("WRONG EVENT BLOCK") return end
+        os.sleep(0.2)
+        if block_id ~= o.block then d("NOTMYBLOCK") return end
+        d(s({o.override, occupied}))
+        if occupied then
+            o.override = true
+        else
+            o.override = false
+        end
+        d(s({o.override, occupied}))
+        o.updateAspect()
+    end
+
+    o._blockEvent = event.listen(BLOCK_UPDATE_SIGNAL, o.handleBlockUpdate)
+    o._aspectEvent = event.listen(ASPECT_UPDATE_SIGNAL, o.handleAspect)
+
     -- should set up other variables here, also event handler functions
+    o.updateAspect()
     return o
 end
 
